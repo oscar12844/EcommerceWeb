978a23612a291f2d89f95a65e62c0a73ce164bad
Dominic Chow <dominicchow83@yahoo.com.hk>
2023-04-09 23:19:39 +0800

    12333

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,21 +1,21 @@
-import React from "react"
-import Back from "../common/back/Back"
-import PriceCard from "./PriceCard"
-import "./price.css"
-import Faq from "./Faq"
+import React from "react";
+import Back from "../common/back/Back";
+import PriceCard from "./PriceCard";
+import "./price.css";
+import Faq from "./Faq";
 
 const Pricing = () => {
   return (
     <>
-      <Back title='Choose The Right Plan' />
-      <section className='price padding'>
-        <div className='container grid'>
+      <Back title="Choose The Native Speaker" />
+      <section className="price padding">
+        <div className="container grid">
           <PriceCard />
         </div>
       </section>
       <Faq />
     </>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
