a955820bc61248486b4a0a291888685eafce3405
Dominic Chow <dominicchow83@yahoo.com.hk>
2023-04-10 14:24:32 +0800

    123

--- src/components/Realtime/PairSpeakerCard.jsx
+++ src/components/Realtime/PairSpeakerCard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { realtimespeaker } from "../../dummydata";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const PairSpeakerCard = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       {realtimespeaker.map((val) => (
@@ -11,7 +26,27 @@ const PairSpeakerCard = () => {
             <img src={val.cover} alt="" />
           </h1>
           <p>{val.desc}</p>
-          <button className="outline-btn">START PAIR!</button>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            START PAIR!
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Join the Zoom Meeting to talk with the Speaker!"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {"Zoom ID: 7752 068 976"}
+              </DialogContentText>
+              <DialogContentText id="alert-dialog-description">
+                {"Zoom Password: R7z1X9QyjK"}
+              </DialogContentText>
+            </DialogContent>
+          </Dialog>
         </div>
       ))}
     </>
